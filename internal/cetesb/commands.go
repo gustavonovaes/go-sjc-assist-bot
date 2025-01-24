1fb764f59ca7248668bfe31e128402665477a8e0
Gustavo Novaes <gustavonovaes.dev@gmail.com>
2025-01-24 19:24:00 -0300

    fix: remove legend quote from air quality index response for cleaner output

--- internal/cetesb/commands.go
+++ internal/cetesb/commands.go
@@ -25,12 +25,10 @@ func CommandQualidadeAr(message telegram.WebhookMessage) error {
 		message.Chat.ID,
 		fmt.Sprintf(
 			`<b>Nome:</b>\n%s\n
-			 <b>Indice qualidade do Ar:</b>\n%.0f - %s\n
-			 %s\n`,
+			 <b>Indice qualidade do Ar:</b>\n%.0f - %s\n`,
 			res.Features[0].Attributes.Nome,
 			res.Features[0].Attributes.Indice,
 			res.Features[0].Attributes.Qualidade,
-			"<quote>[ 0-40: Boa / 41-80: Moderada / 81-120: Ruim / 121-200: Muito Ruim / >200: Péssima ]</quote>\n",
 		),
 	)
 }
